96f04bc4ee3ceb602a2e66297f193460ca2d0a67
Mo_husen <mhusenhaji0@gmail.com>
2023-11-02 21:41:24 +0530

    mad_p10_026

---
+++ app/src/main/java/com/mo_husen/mad_pra10_026/PersonListCardModel.kt
@@ -0,0 +1,26 @@
+package com.mo_husen.mad_pra10_026
+import org.json.JSONObject
+import java.io.Serializable
+
+class PersonListCardModel(
+    jsonObject: JSONObject): Serializable {
+    var id:String
+    var name: String
+    var emailld: String
+    var phoneNo: String
+    var address: String
+    var latitude: Double
+    var longitude:Double
+
+    init {
+        id = jsonObject.getString("id")
+        emailld = jsonObject.getString("email")
+        phoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        name = profileJson.getString("name")
+        address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        latitude = locationJson.getDouble("lat")
+        longitude = locationJson.getDouble("long")
+    }
+}
